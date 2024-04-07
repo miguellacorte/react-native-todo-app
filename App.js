2145cfca7ac2388db58d9ea006ec23f13af37dfc
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2024-04-07 12:11:59 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.goalContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="your course goal!"
+        ></TextInput>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: "white" }}>Add goal</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.text}> hello world!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    borderRadius: 10,
+    height: 40,
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 10,
+    marginRight: 30,
+    color: "blue",
+    borderRadius: 10,
+    backgroundColor: "blue",
+    borderWidth: 1,
+    borderColor: "blue",
+    padding: 10,
+    width: "30%",
+  },
+  goalContainer: {
+    margin: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 15,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "red",
+    fontSize: 20,
   },
 });
