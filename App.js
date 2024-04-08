cd79128e28a9f44e4ae8832ca8967990121cc1b2
miguellacorte <76566384+miguellacorte@users.noreply.github.com>
2024-04-08 13:20:58 -0400

    delete last item in llisst button implemented

--- App.js
+++ App.js
@@ -1,4 +1,11 @@
-import { StyleSheet, View, SafeAreaView, FlatList } from "react-native";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  FlatList,
+  Text,
+  Pressable,
+} from "react-native";
 
 import GoalRender from "./components/GoalRender";
 import GoalInput from "./components/GoalInput";
@@ -7,7 +14,6 @@ import React, { useState } from "react";
 
 export default function App() {
   const [goals, setGoals] = useState([]);
-  
 
   function addGoalHandler(enteredGoalText) {
     //best practices on state update is your state depends on the previous state:
@@ -21,11 +27,17 @@ export default function App() {
       { text: enteredGoalText, key: Math.random().toString() },
     ]);
   }
+
+  function deleteGoalHandler() {
+    setGoals((currentCourseGoals) => {
+      const newGoals = [...currentCourseGoals]; // create a copy of the current goals
+      newGoals.pop(); // remove the last item
+      return newGoals; // return the updated array
+    });
+  }
   return (
     <SafeAreaView style={styles.appContainer}>
-      <GoalInput
-        goalHandleFunction={addGoalHandler}
-      />
+      <GoalInput goalHandleFunction={addGoalHandler} />
 
       <View style={styles.bottomContainer}>
         <FlatList
@@ -35,11 +47,38 @@ export default function App() {
           }}
         ></FlatList>
       </View>
+      <Pressable style={styles.deleteContainer} onPress={deleteGoalHandler}>
+        <View style={styles.deleteContainerView}>
+          <Text style={styles.deleteContainerText}>
+            Delete last item in list
+          </Text>
+        </View>
+      </Pressable>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  deleteContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingBottom: 30,
+  },
+  deleteContainerView: {
+    width: "50%",
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: "#cccccc",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  deleteContainerText: {
+    color: "black",
+  },
   appContainer: {
     flex: 1,
     padding: 10,
